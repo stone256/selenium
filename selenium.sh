15c71b52d44823ab7924a499ba8ea3285fbc95c4
Your Name <you@example.com>
2020-02-18 10:48:33 +1100

    a

--- selenium.sh
+++ selenium.sh
@@ -12,5 +12,5 @@
 #Xvfb :10 -screen 5 1920x5200x8 -ac </dev/null >/dev/null 2>&1 &
 #export DISPLAY=:10.5
 
-cd /var/www/html/IODS6/selenium && nohup java -Dwebdriver.gecko.drive -Dwebdriver.chrome.drive -jar selenium-server-standalone-3.141.59.jar -port 1030 </dev/null >/dev/null 2>&1 &
+cd /var/www/html/IODS6/selenium && nohup java -Dwebdriver.gecko.drive -Dwebdriver.chrome.drive -jar selenium-server-standalone-3.141.59.jar -port xxxx </dev/null >/dev/null 2>&1 &
 
